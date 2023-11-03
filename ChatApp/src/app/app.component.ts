d15a7db8045f6c42234b85cf5f090b36fe37d598
Mohammed-Yassine BOUMEHDI <myboumehdi.2002@gmail.com>
2023-11-03 02:54:30 +0100

    pollyfils pb

--- ChatApp/src/app/app.component.ts
+++ ChatApp/src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs-client';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ChatApp';
+
+export class AppComponent implements OnInit {
+  title = "ChatApp";
+  newmessage!: string;
+  publicMessages: string[] = [];
+  private stompClient!: Stomp.Client;
+
+  ngOnInit() {
+      this.connect();
+  }
+
+  connect() {
+    const socket = new SockJS('http://localhost:9092/ws');
+    this.stompClient = Stomp.over(socket);
+    const _this = this;
+    this.stompClient.connect({}, function (frame) {
+      _this.stompClient.subscribe('/topic', function(received){
+        _this.showMessage(JSON.parse(received.body));
+      });
+   });
+  }
+  
+  sendMessage() {
+    this.stompClient.send(
+      '/app/public',
+      {},
+      JSON.stringify(this.newmessage)
+    );
+    this.newmessage = "";
+  }
+
+  showMessage(message: string) {
+    this.publicMessages.push(message);
+  }
+
 }
